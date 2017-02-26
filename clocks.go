12fd1c593f561da5b7637702514e283c651cecd8
David Stotijn <dstotijn@gmail.com>
2017-02-26 10:41:03 +0100

    Draw clocks with minutes and hours

--- clocks.go
+++ clocks.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"time"
 
@@ -45,30 +46,46 @@ func run(g *grid) error {
 	defer w.Destroy()
 	defer r.Destroy()
 
-	if err := r.Clear(); err != nil {
-		return fmt.Errorf("could not clear renderer: %v", err)
-	}
+	return loop(r, g)
+}
 
-	for dx := range *g {
-		for dy := range (*g)[dx] {
-			if err := (*g)[dx][dy].draw(r, dx, dy); err != nil {
-				return fmt.Errorf("could not draw clock: %v", err)
+func loop(r *sdl.Renderer, g *grid) error {
+	for {
+		r.SetDrawColor(0, 0, 0, 255)
+		if err := r.Clear(); err != nil {
+			return fmt.Errorf("could not clear renderer: %v", err)
+		}
+		for dx := range *g {
+			for dy := range (*g)[dx] {
+				if err := (*g)[dx][dy].draw(r, dx, dy); err != nil {
+					return fmt.Errorf("could not draw clock: %v", err)
+				}
+				(*g)[dx][dy].minute++
 			}
 		}
+		r.Present()
+		time.Sleep(25 * time.Millisecond)
 	}
-
-	r.Present()
-	time.Sleep(time.Second * 5)
-
-	return nil
 }
 
 func (c *clock) draw(r *sdl.Renderer, dx, dy int) error {
 	x := dx*2*(clockRadius+gutter) + clockRadius*2
 	y := dy*2*(clockRadius+gutter) + clockRadius*2 - gutter
 	color := sdl.Color{R: 255, G: 255, B: 255, A: 255}
-	if result := gfx.CircleColor(r, x, y, clockRadius, color); result == false {
+	if result := gfx.AACircleColor(r, x, y, clockRadius, color); result == false {
 		return fmt.Errorf("could not draw circle: %v", sdl.GetError())
+	}
+
+	mx := x + int(clockRadius*0.8*math.Cos(3*math.Pi/2+((float64(c.minute)/60.0)*2*math.Pi))-0.5)
+	my := y + int(clockRadius*0.8*math.Sin(3*math.Pi/2+((float64(c.minute)/60.0)*2*math.Pi))-0.5)
+	if result := gfx.AALineColor(r, x, y, mx, my, color); result == false {
+		return fmt.Errorf("could not draw minute hand: %v", sdl.GetError())
+	}
+
+	hx := x + int(clockRadius*0.6*math.Cos(3*math.Pi/2+((float64(c.hour)/60.0)*2*math.Pi))-0.5)
+	hy := y + int(clockRadius*0.6*math.Sin(3*math.Pi/2+((float64(c.hour)/60.0)*2*math.Pi))-0.5)
+	if result := gfx.AALineColor(r, x, y, hx, hy, color); result == false {
+		return fmt.Errorf("could not draw minute hand: %v", sdl.GetError())
 	}
 
 	return nil
